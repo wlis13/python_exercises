7bc1d399db24fdc0064e137ce1309a644bd0924a
wlisses <wlissesfernando285@gmail.com>
2023-05-08 14:25:33 -0300

    criada função buy_fan para compra

--- padroes-de-projeto/Dia-01/client.py
+++ padroes-de-projeto/Dia-01/client.py
@@ -1,11 +1,32 @@
 class Client:
-    def __init__(self, name, cpf, identity, phone, address, number_address):
+    def __init__(self,
+                 name,
+                 cpf,
+                 identity,
+                 phone,
+                 address,
+                 number_address,
+                 account_value):
         self._name = name
         self._cpf = cpf
         self._identity = identity
         self._phone = phone
         self._address = address
         self._number_address = number_address
+        self._account_value = account_value
+        self._has_a_fan = False
+
+    def __str__(self):
+        return f"""
+            name: {self._name}
+            cpf: {self._cpf}
+            identity: {self._identity}
+            phone: {self._phone}
+            address: {self._address}
+            number_address: {self._number_address}
+            account_value: {self._account_value}
+            has_a_fan: {self._has_a_fan}
+        """
 
 
 first_client = Client(
@@ -14,18 +35,21 @@ first_client = Client(
     'SC: 01.248.369',
     '(31) 94587-2514',
     'Rua: Delfino Duarte/Dom Bosco',
-    1478)
+    1478,
+    250.50)
 second_client = Client(
     'Adriana Gonçalves',
     '147.288.369-12',
     'MG: 9.562.348',
     '(33) 98747-1256',
     'Rua: Reginal Amaral/Sintia',
-    146)
+    146,
+    20000.00)
 thirth_client = Client(
     'Eliza Martins',
     '956.236.158-47',
     'MS: 14.123.321',
     '(25) 97845-2365',
     'Rua: Rezende Siqueira/Santo Antônio',
-    958)
+    958,
+    740.20)
--- padroes-de-projeto/Dia-01/fan.py
+++ padroes-de-projeto/Dia-01/fan.py
@@ -1,3 +1,6 @@
+import client
+
+
 class Fan:
     def __init__(self, price, brand, power, voltage, size, maximum_rotation):
         self._price = price
@@ -7,7 +10,26 @@ class Fan:
         self._size = size
         self._maximum_rotation = maximum_rotation
 
+    def buy_fan(self, clients):
+        if clients._account_value < self._price:
+            raise ValueError("desculpe mais sua conta não tem recurso suficiente")
+        clients.has_a_fan = True
+        return clients
+
+    def __str__(self):
+        return f"""
+            price: {self._price}
+            brand: {self._brand}
+            power: {self._power}
+            voltage: {self._voltage}
+            size: {self._size}
+            maximum_rotation: {self._maximum_rotation}
+        """
+
 
 first_fan = Fan(250.98, 'toshiba', 1200, 110, 'pequeno', 300)
 second_fan = Fan(354.98, 'max wind', 1500, 110, 'médio', 430)
 thirth_fan = Fan(550.90, 'max wind', 1800, 110, 'grande',  650)
+
+first_buy = first_fan.buy_fan(client.first_client)
+print(first_buy)
